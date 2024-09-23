d8f24eeb408ae7d178d5d130c6e576569cc70df8
antkas <82690201+kasljevic@users.noreply.github.com>
2024-09-24 00:24:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,50 @@ const answerContainer = document.getElementById('answerContainer');
 const answer = document.getElementById('answer');
 const jokeImage = document.getElementById('jokeImage');
 
-const joke = {
-    question: "Varför gick kuken över vägen?",
-    answer: "För att komma till andra sidan!"
-};
+const jokes = [
+    {
+        question: "Varför gick kuken över vägen?",
+        answer: "För att komma till andra sidan!"
+    },
+    {
+        question: "Vad sa kuken när den flög över staketet?",
+        answer: "Kuckeliku!"
+    },
+    {
+        question: "Hur kallar man en kuk som är bra på matte?",
+        answer: "En räknehane!"
+    },
+    {
+        question: "Varför hade kuken på sig solglasögon?",
+        answer: "För att han inte ville bli igenkänd!"
+    },
+    {
+        question: "Vad gör en kuk på ett bibliotek?",
+        answer: "Läser hönsböcker!"
+    },
+    {
+        question: "Hur tränar en kuk?",
+        answer: "Med tupplöpning!"
+    }
+];
 
+let currentJoke;
 let isJokeVisible = false;
+
+function getRandomJoke() {
+    return jokes[Math.floor(Math.random() * jokes.length)];
+}
 
 jokeButton.addEventListener('click', () => {
     if (isJokeVisible) {
         jokeContainer.classList.add('hidden');
         answerContainer.classList.add('hidden');
-        jokeButton.textContent = 'Visa kuken-skämt';
+        jokeButton.textContent = 'Visa nytt kuken-skämt';
         isJokeVisible = false;
     } else {
+        currentJoke = getRandomJoke();
         jokeContainer.classList.remove('hidden');
-        question.textContent = joke.question;
+        question.textContent = currentJoke.question;
         answerButton.classList.remove('hidden');
         jokeButton.textContent = 'Dölj skämt';
         isJokeVisible = true;
@@ -30,7 +58,7 @@ jokeButton.addEventListener('click', () => {
 
 answerButton.addEventListener('click', () => {
     answerContainer.classList.remove('hidden');
-    answer.textContent = joke.answer;
+    answer.textContent = currentJoke.answer;
     jokeImage.src = 'https://ggscore.com/media/logo/t62288.png?75';
     answerButton.classList.add('hidden');
 });
